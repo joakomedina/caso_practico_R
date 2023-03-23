2a0d38ee1cdad689e77456c0688511307b11135f
joakomedina <joaquinaarvelom@gmail.com>
2023-03-23 14:22:10 +0100

    Análisis del patrón de sueño tras la limpieza y formateo del data.frame

---
+++ scripts/patron_sueño.R
@@ -0,0 +1,53 @@
+
+## Creación de data.frame sleep_day_df
+
+sleep_day_df <- 
+  read_csv("C:\\Users\\Joako\\Documents\\R_curso_analisis_datos\\Caso_practico_R\\data\\sleepDay_merged.csv")
+head(sleep_day_df)
+
+### Limpieza, exploración de datos y registros, formateo y estandarización
+
+###### Verificación registros de usuarios
+
+n_unique(sleep_day_df$Id)
+
+###### Verificación de duplicados
+
+sum(duplicated(sleep_day_df))
+
+###### Removiendo duplicados
+
+sleep_day_df <- sleep_day_df %>%
+  distinct() %>%
+  drop_na()
+
+###### Limpieza nombres de columnas
+
+clean_names(sleep_day_df)
+
+###### Formateo a minúsculas 
+
+sleep_day_df <- rename_with(sleep_day_df, tolower)
+
+###### Formateo de la columna activity_date a tipo date y renombrado de columna a date y división de la columna date para que solo
+###### tenga datos fecha y creación de columna time para que contenga el registro de tiempo
+
+sleep_day_df <- sleep_day_df %>%
+  rename(date = sleepday)
+
+sleep_day_df <- sleep_day_df %>%
+  mutate(date = as.POSIXct(date,format ="%m/%d/%Y %I:%M:%S %p" , tz=Sys.timezone()))
+
+sleep_day_df$time <- format(sleep_day_df$date, format = "%H:%M:%S")
+sleep_day_df$date <- format(sleep_day_df$date, format = "%m-%d-%y")
+
+head(sleep_day_df)
+
+## Resúmen horas de sueño
+
+sleep <- sleep_day_df %>%
+  select( totalsleeprecords, totalminutesasleep, totaltimeinbed) %>%
+  summary()
+View(sleep)
+
+
