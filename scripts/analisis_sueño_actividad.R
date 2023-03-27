08512907137b3b7d098a3c6f1a7af9ea2820cb1a
joakomedina <joaquinaarvelom@gmail.com>
2023-03-27 13:35:00 +0200

    Versión final

--- scripts/analisis_sueño_actividad.R
+++ scripts/analisis_sueño_actividad.R
@@ -8,4 +8,12 @@ sleep_activity <- activity_sleep %>%
   select(id, date, totalsleeprecords, totalsteps) %>%
   group_by(totalsleeprecords) %>%
   summarise(avg = mean(totalsteps))
+
 View(sleep_activity)
+
+#### Visualizar relación
+
+ggplot(sleep_activity, aes(x = totalsleeprecords, y = avg)) +
+  geom_bar(stat = "identity", fill = "#76b7b2") +
+  labs(title = "Promedio de pasos dados según el número de registros de sueño",
+       x = "Número de registros de sueño", y = "Promedio de pasos dados")
