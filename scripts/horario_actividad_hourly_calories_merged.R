14a5ab8c1320ef77de46b33b4c2b9a9c8fe21b9f
joakomedina <joaquinaarvelom@gmail.com>
2023-03-29 00:44:34 +0200

    fue modificado el script porque no me conducía a nada nuevo, ahora se llama intensities_calories en donde se ve la relación entre intensidad y gasto calórico

--- scripts/horario_actividad_hourly_calories_merged.R
+++ scripts/horario_actividad_hourly_calories_merged.R
@@ -1,5 +1,10 @@
+
 # Este es el scrip con el data frame para analizar los horarios de mayor actividad física según los registros de los dispositivos
 # Este análisis se realiza con la base de datos hourlyCalories_merged.csv
+# El énfasis esta en el promedio de gasto calórico durante el día en cada hora
+
+# Sin embargo la mayor utilidad de este data frame es la combinación con hourly_intensity para establecer una 
+# relación entre la intensidad de la actividad física y el gasto calórico
 
 hourly_activity <- read_csv("C:\\Users\\Joako\\Documents\\R_proyectos\\Caso_practico\\data_trabajo\\hourlyCalories_merged.csv") 
 head(hourly_activity)
@@ -19,16 +24,43 @@ hourly_activity <- hourly_activity %>%
 hourly_activity$time <- format(hourly_activity$date, format = "%H:%M:%S")
 hourly_activity$date <- format(hourly_activity$date, format = "%m-%d-%y")
 
-class(hourly_activity$date)
 
-hourly_activity <- hourly_activity %>%
+# Hasta aqui el formateo con columnas date y time en formato Date y ITime
+# El siguiente análisis no me dice nada diferente que hourly_intensities con lo cuál:
+  # El summarise creo que no es útil, me lleva a un gráfico final igual que el de hourly_intensities pero desde las calorías
+  # Puedo hacer un merge con hourly_intensities para relacionar intensidad con gasto calórico.
+
+hourly <- hourly_activity %>%
   group_by(time) %>%
   summarise(avg_intensity = mean(calories))
+  
+# No es necesario a menos que vuelva ITime la columna time antes, es por ello que después del merge es que las voy a volver time
+      # Me da un error en el gráfico para una escala automatica con objetos de type ITime 
+      # error: Don't know how to automatically pick scale for object of type <ITime>. Defaulting to continuous.
+      # cambio a time posicxct
 
+# hourly$time <- as.POSIXct(hourly$time, format = "%H:%M:%S")
 
-ggplot(data=hourly_activity, aes(x=time, y=avg_intensity)) + geom_bar(stat = "identity", fill= '#76b7b2')+
+ggplot(hourly, aes(x=time, y= avg_intensity)) + 
+  geom_bar(stat = "identity", fill= '#76b7b2')+
   theme(axis.text.x = element_text(angle = 90))+
   labs(title = "Horas de mayor actividad física ",
        x = "Hora", y = "Gasto calorico medio")
 
 
+# Merge con hourly
+
+intensities_calories <- merge(hourly_activity, hourly_intensities, by =c("id", "date", "time"))
+
+        # Ponerlo en suspenso a menos que sea necesario para el análisis         
+          ## hourly_activity$date <- as.Date(strptime(hourly_activity$date, format = "%m-%d-%Y"))
+          ## class(hourly_activity$date)
+          
+          ## hourly_activity$time <- as.ITime(hourly_activity$time, format = "%H:%M:%S")
+          ## class(hourly_activity$time)
+
+
+ggplot(data=intensities_calories)+
+  geom_smooth(mapping = aes(x=calories, y= averageintensity), color="#76b7b2")+
+  labs(title = "Relación entre la intensidad de la actividad física y el gasto calórico",
+       x = "Gasto Calórico", y = "Intensidad de actividad")
