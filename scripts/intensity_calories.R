e207ba99ea2dcd40726b08a340f42d477d691c9c
joakomedina <joaquinaarvelom@gmail.com>
2023-03-29 00:58:40 +0200

    ignorar

---
+++ scripts/intensity_calories.R
@@ -0,0 +1,6 @@
+#Este script estoy realizando un análisis de intensidad vs calorías para hallar algún patron. Aqui utilice tres tablas
+min_calories_narrow <- 
+  read_csv("C:\\Users\\Joako\\Documents\\R_curso_analisis_datos\\caso_practico_R\\data\\minuteCaloriesNarrow_merged.csv")
+
+min_intensities_narrow <- 
+  read_csv("C:\\Users\\Joako\\Documents\\R_proyectos\\Caso_practico\\data_trabajo\\minuteIntensitiesNarrow_merged.csv")
