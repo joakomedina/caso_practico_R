ba3296100e70b2e088ce6b24a3e3c2d9b9ea4d9c
joakomedina <joaquinaarvelom@gmail.com>
2023-03-29 15:15:41 +0200

    Finalmente decido trabajar con min_calories_narrow e intensities pero como ya tenia un data.frame creado desde un merge quedo en otro script

--- scripts/intensity_calories.R
+++ scripts/intensity_calories.R
@@ -1,6 +1,8 @@
-#Este script estoy realizando un análisis de intensidad vs calorías para hallar algún patron. Aqui utilice tres tablas
+#Este script estoy realizando un análisis de intensidad vs calorías para hallar algún patron. La idea es comparar al sujeto contra si
+# mismo en la quemada de calorías diarias. Este proceso debe hacer un merge entre min_intensities_narrow y min_calories_narrow
+# como ya tengo min_intensities_narrow procesada en otro analisis solo me queda procesar min_calories_narrow
+
+
 min_calories_narrow <- 
   read_csv("C:\\Users\\Joako\\Documents\\R_curso_analisis_datos\\caso_practico_R\\data\\minuteCaloriesNarrow_merged.csv")
 
-min_intensities_narrow <- 
-  read_csv("C:\\Users\\Joako\\Documents\\R_proyectos\\Caso_practico\\data_trabajo\\minuteIntensitiesNarrow_merged.csv")
